274eec548ffa4c85eb0d38bbef68c69dd48a028d
Rubangakene Inno Joseph <105200997+rgb-deezoh@users.noreply.github.com>
2022-09-01 12:41:48 +0300

    Comparison with tags
    collision comparison using tags and camera position adjustments

--- Assets/Challenge 2/Scripts/DetectCollisionsX.cs
+++ Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class DetectCollisionsX : MonoBehaviour
 {
-
     private void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("Ground"))
+        {
+            Debug.Log("Game Over");
+        }
         Destroy(gameObject);
     }
 }
